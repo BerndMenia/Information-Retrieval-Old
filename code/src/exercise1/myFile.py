f774788e6c428ca4efc66d34fcce338cde54c7e3
BerndMenia <bernd_menia@yahoo.de>
2019-03-18 19:41:58 +0100

    Added a similar function to frequency(), frequency_top_50() that only returns the top 50 most common terms and with their respective ranking in a list of tuples.

--- code/src/exercise1/myFile.py
+++ code/src/exercise1/myFile.py
@@ -4,7 +4,6 @@ from collections import Counter
 import string
 
 
-
 # https://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
 # https://stackoverflow.com/questions/9427037/relative-path-not-working-even-with-init-py
 def read_in(file_path):
@@ -31,12 +30,23 @@ def unique_terms(text=""):
 
 
 # https://stackoverflow.com/questions/40985203/counting-letter-frequency-in-a-string-python
+# https://stackoverflow.com/questions/20950650/how-to-sort-counter-by-value-python
 def calculate_frequency(s):
     counts = Counter(s)  # Counter({'l': 2, 'H': 1, 'e': 1, 'o': 1})
     counts.most_common()
     # for i in s:
     #     print(i, counts[i])
     return counts
+
+
+def get_top_50(frequency):
+    lst = frequency.most_common(50)
+
+    for i in range(len(lst)):
+        term, amount = lst[i]
+        lst[i] = (i+1, term, amount)
+
+    return lst
 
 
 text = read_in("books/the_raven.txt")
@@ -51,5 +61,9 @@ text_split = text.split()
 print("Total number of unique terms:", len(unique_terms(text_split)))
 
 frequency = calculate_frequency(text_split)
-print(frequency)
-print(type(frequency))
+print("Frequencies:", frequency)
+
+# Returns a list in contrast to calculate_frequency()!
+frequency_top_50 = get_top_50(frequency)
+print("Frequencies top 50:", frequency_top_50)
+print("Type:", type(frequency_top_50))
