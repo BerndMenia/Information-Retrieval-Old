fc5b70d6da551947d2367a83fbcbc0dfc737b054
BerndMenia <bernd_menia@yahoo.de>
2019-03-18 19:09:52 +0100

    Added functions to count the number of terms and unique terms.

--- code/src/exercise1/myFile.py
+++ code/src/exercise1/myFile.py
@@ -1,18 +1,45 @@
 from os.path import abspath
+from collections import Counter
+
 import string
 
 
+
+# https://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+# https://stackoverflow.com/questions/9427037/relative-path-not-working-even-with-init-py
 def read_in(file_path):
     file_path = abspath("../../../" + file_path)
     file = open(file_path, "r")
     return file.read()
 
 
+# https://stackoverflow.com/questions/265960/best-way-to-strip-punctuation-from-a-string-in-python
 def remove_punctuation(s):
     return s.translate(str.maketrans('', '', string.punctuation))
 
+
+def total_number_of_terms(s):
+    return len(s.split())
+
+
+def unique_words(text=""):
+    lst = []
+    for s in text:
+        if not s in lst:
+            lst.append(s)
+    return lst
+
+
+# def calculate_frequency(s):
+
+
 text = read_in("books/the_raven.txt")
-#print(text)
+# print(text)
 
 text = remove_punctuation(text)
-print(text)
+# print(text)
+
+print("Total number of terms:", total_number_of_terms(text))
+
+text_split = text.split()
+print("Total number of unique terms:", len(unique_words(text_split)))
